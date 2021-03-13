5c35a4dd6c636a815079eb45dab440f7d150f553
edoardo <collo>
2021-03-13 15:44:20 +0100

    add categories table an relations drop types table an relation

--- database/migrations/2021_03_13_142455_create_categories_table.php
+++ database/migrations/2021_03_13_142455_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTypesTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateTypesTable extends Migration
      */
     public function up()
     {
-        Schema::create('types', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
+        Schema::create('categories', function (Blueprint $table) {
+          $table->id();
+          $table->string('name');
+          $table->timestamps();
         });
     }
 
@@ -27,6 +27,6 @@ class CreateTypesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('types');
+        Schema::dropIfExists('categories');
     }
 }
