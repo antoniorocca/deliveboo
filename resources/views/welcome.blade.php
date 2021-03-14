2a47002e96b3e3b24d4d52866db173645d5caefa
matteobuscicchio <matteoezio@me.com>
2021-03-14 13:12:44 +0100

    aggiunto percorso semplice per andare in /user/home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/user/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
