62f55dba8be9f088a0fbe5c2cbde9327815d6190
edoardo <collo>
2021-03-22 15:02:26 +0100

    style dishes crud2

--- resources/views/users/dish/show.blade.php
+++ resources/views/users/dish/show.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.app')
 @section('content')
 {{-- {{dd($dish)}} --}}
-<div class="container">
+<div class="spacer">
+  <img class="img_spacer" src="{{asset('storage/lightgreen_wave.png')}}" alt="">
+</div>
+
+<div id="backend_container"class="">
+<div class="backend_wrap">
     <a class="btn btn-primary" href="{{ route('user.dish.index') }}">Dishes</a>
-    <h1>piatto: {{$dish->name}}</h1>
-    <div class="card">
-        <div class="card-body">
+    <h1>Piatto: {{$dish->name}}</h1>
+
+
             <h4 class="text-info">img</h4>
             @if ($dish->img)
             <img src="{{asset('storage/' . $dish->img )}}" style="height:300px;" alt="">
@@ -13,36 +18,48 @@
             @else
             <p class="card-text text-danger">N/A</p>
             @endif
-            <h4 class="text-info">Nome</h4>
-            <p class="card-title text-danger">{{$dish->name}}</p>
-            <h4 class="text-info">descrizione</h4>
-            <p class="card-text text-danger">
+            <div class="content_row">
+              <h4 class="text-info">Nome</h4>
+              <p class="card-title text-danger">{{$dish->name}}</p>
+            </div>
+            <div class="content_row">
+              <h4 class="text-info">descrizione</h4>
+              <p class="card-text text-danger">
                 @if ($dish->description)
                 {{$dish->description}}
                 @else
                 N/A
                 @endif
-            </p>
-            <h4 class="text-info">prezzo</h4>
-            <p class="card-text">{{$dish->price}}</p>
-            <h4 class="text-info">classe menu</h4>
-            <p class="card-text">
+              </p>
+            </div>
+            <div class="content_row">
+              <h4 class="text-info">prezzo</h4>
+              <p class="card-text">{{$dish->price}}</p>
+            </div>
+            <div class="content_row">
+              <h4 class="text-info">classe menu</h4>
+              <p class="card-text">
                 @if ($dish->menu_class)
                 {{$dish->menu_class}}
                 @else
                 N/A
                 @endif
-            </p>
-
-            <h4 class="text-info">sconto</h4>
-            <p class="card-text text-danger">
+              </p>
+            </div>
+            <div class="content_row">
+              <h4 class="text-info">sconto</h4>
+              <p class="card-text text-danger">
                 @if ($dish->discount === 0)
                 no
                 @else
                 QUI STAMPARE SCONTO
                 @endif
-            </p>
-        </div>
-    </div>
+              </p>
+            </div>
+
+
+
+
+</div>
 </div>
 @endsection
