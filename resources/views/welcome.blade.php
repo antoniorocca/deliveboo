600f8e36a2a772b0356048dee3905c1089718af7
Rocco Orefice <rocco.orefice92@gmail.com>
2021-03-24 13:34:36 +0100

    structure html

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,42 @@
 <div class="wrap" style="display:flex;">
     <selected-restaurant></selected-restaurant>
 
+    <!-- CALL-TO-ACTION -->
+    <div class="call_to_action">
+        <div class="title">
+            <h2>Diventa uno di noi!</h2>
+        </div>
+        <div class="container_calls container">
+            <div class="single_call">
+                <div class="box_img">
+                    <img src="{{ asset("img/photo_rider.jpg") }}" alt="" srcset="">
+                </div>
+                <h4>Diventa un rider</h4>
+                <p>Lavora per te stesso! Goditi flessibilità, libertà e guadagni competitivi effettuando consegne con Deliveboo.</p>
+            </div>
+
+            <div class="single_call">
+                <div class="box_img">
+                    <img src="{{ asset("img/photo_rider.jpg") }}" alt="" srcset="">
+                </div>
+                <h4>Diventa un rider</h4>
+                <p>Lavora per te stesso! Goditi flessibilità, libertà e guadagni competitivi effettuando consegne con Deliveboo.</p>
+            </div>
+
+            <div class="single_call">
+                <div class="box_img">
+                    <img src="{{ asset("img/photo_rider.jpg") }}" alt="" srcset="">
+                </div>
+                <h4>Diventa un rider</h4>
+                <p>Lavora per te stesso! Goditi flessibilità, libertà e guadagni competitivi effettuando consegne con Deliveboo.</p>
+            </div>
+            
+        </div>
+    
+
+
+    </div>
+
     <!-- CARRELLO -->
     <div style="display:flex; flex-direction:column;">
         <cart-dropdown id="box_cart" class="cart-box"></cart-dropdown>
