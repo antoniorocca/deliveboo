91530aff16239028b0a8613e0787ab43d137d46f
edoardo <edoardo.collo@gmail.com>
2021-03-24 01:52:15 +0100

    fix dish create

--- resources/views/users/dish/create.blade.php
+++ resources/views/users/dish/create.blade.php
@@ -14,18 +14,15 @@
         <span>* campo obligatorio</span>
 
         {{-- name --}}
-        <div class="content_row">
-            <label for="name" class="col-sm-1-12 col-form-label text-info">name*</label>
+            <label for="name" class="col-sm-1-12 col-form-label text-info">name</label>
             <div class="col-md-12-12 col-md-12">
                 <input type="text" class="edit_input" name="name" value="" maxlength='30'>
             </div>
             @error('name')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-        </div>
 
         {{-- description --}}
-        <div class="content_row">
             <label for="description" class="col-sm-1-12 col-form-label text-info">Description</label>
             <div class="col-md-12-12 col-md-12">
                 <textarea type="text" class="edit_input" name="description" value="" maxlength='500'></textarea>
@@ -33,21 +30,17 @@
             @error('description')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-        </div>
 
         {{-- img --}}
-        <div class="form-group">
             <label for="img">immagine piatto</label>
             <input type="file" class="edit_input-file" name="img" aria-describedby="fileHelpId">
             @error('img')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-        </div>
 
-
+            <br>
 
         {{-- price --}}
-        <div class="content_row">
             <label for="price" class="col-sm-1-12 col-form-label text-info">Price*</label>
             <div class="col-md-12-12 col-md-12">
                 <input type="number" class="edit_input" name="price" value="" min="0" max="999.99" step="0.01">
@@ -55,10 +48,8 @@
             @error('price')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-        </div>
 
         {{-- menu_class --}}
-        <div class="content_row">
             <label for="menu_class" class="col-sm-1-12 col-form-label text-info">menu_class</label>
             <div class="col-md-12-12 col-md-12">
                 <textarea type="text" class="edit_input" name="menu_class" value="" maxlength='30'></textarea>
@@ -66,10 +57,8 @@
             @error('menu_class')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-        </div>
 
         {{-- discount --}}
-        <div class="content_row">
             <label for="discount" class="col-lg-12 col-form-label text-info">discount</label>
 
             <div class="col-md-1">
@@ -84,18 +73,15 @@
             @error('discount')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-        </div>
 
 
 
 
         {{-- Create button --}}
-        <div class="content_row">
             <div class="offset-sm-2 col-sm-10">
                 <input id="glovo_btn" type="submit" value="Create">
             </div>
         </div>
-    </div>
 </form>
 </div>
 </div>
