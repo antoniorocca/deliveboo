6fa041dd0d4c05ccf82a065da405478b7c96d934
edoardo <edoardo.collo@gmail.com>
2021-03-25 13:17:01 +0100

    ss

--- public/js/app.js
+++ public/js/app.js
@@ -40187,11 +40187,11 @@ var render = function() {
           _vm._l(_vm.$store.state.dishes.data.response, function(item) {
             return _c("li", [
               _vm._v(
-                "\r\n      " +
+                "\n      " +
                   _vm._s(item.name) +
-                  "\r\n      " +
+                  "\n      " +
                   _vm._s(item.price) +
-                  "\r\n      "
+                  "\n      "
               ),
               _c(
                 "button",
@@ -40203,7 +40203,7 @@ var render = function() {
                     }
                   }
                 },
-                [_vm._v("Add to Cart\r\n      ")]
+                [_vm._v("Add to Cart\n      ")]
               )
             ])
           }),
@@ -40373,9 +40373,9 @@ var render = function() {
                         _vm._v(" "),
                         _c("h4", [
                           _vm._v(
-                            "\r\n                        " +
+                            "\n                        " +
                               _vm._s(restaurant.name) +
-                              "\r\n                    "
+                              "\n                    "
                           )
                         ]),
                         _vm._v(" "),
@@ -40414,9 +40414,9 @@ var render = function() {
                         _vm._v(" "),
                         _c("h4", [
                           _vm._v(
-                            "\r\n                        " +
+                            "\n                        " +
                               _vm._s(restaurant.name) +
-                              "\r\n                    "
+                              "\n                    "
                           )
                         ]),
                         _vm._v(" "),
@@ -40446,7 +40446,7 @@ var staticRenderFns = [
       _vm._v(" "),
       _c("p", [
         _vm._v(
-          "\r\n                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptatibus?\r\n                "
+          "\n                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptatibus?\n                "
         )
       ])
     ])
@@ -55435,8 +55435,8 @@ var store = {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\gitKraken_esercizi\deliveboo_progetto_finale\deliveboo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\gitKraken_esercizi\deliveboo_progetto_finale\deliveboo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/boolean/laravel/deliveboo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/boolean/laravel/deliveboo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
