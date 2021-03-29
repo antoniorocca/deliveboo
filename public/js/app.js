61cd9f93e20cc96cc25ef7f9b9f6144f87e737c2
edoardo <edoardo.collo@gmail.com>
2021-03-29 11:48:25 +0200

    add orders seeder

--- public/js/app.js
+++ public/js/app.js
@@ -41155,7 +41155,7 @@ var staticRenderFns = [
       _vm._v(" "),
       _c("p", [
         _vm._v(
-          "\r\n            Scopri i ristoranti più richiesti e ricevi alla tua porta ogni tuo desiderio\r\n            "
+          "\n            Scopri i ristoranti più richiesti e ricevi alla tua porta ogni tuo desiderio\n            "
         )
       ])
     ])
@@ -41462,17 +41462,15 @@ var render = function() {
             _c("div", { staticClass: "wrap_category_button" }, [
               _c("span", [
                 _c("strong", [
-                  _vm._v(
-                    "\r\n          " + _vm._s(category.name) + "\r\n        "
-                  )
+                  _vm._v("\n          " + _vm._s(category.name) + "\n        ")
                 ])
               ]),
               _vm._v(" "),
               _c("span", { staticClass: "restaurants_badge" }, [
                 _vm._v(
-                  "\r\n        " +
+                  "\n        " +
                     _vm._s(category.restaurants.length) +
-                    "\r\n      "
+                    "\n      "
                 )
               ])
             ])
@@ -41489,7 +41487,7 @@ var staticRenderFns = [
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
     return _c("div", { staticClass: "wrap_category_button" }, [
-      _c("span", [_c("strong", [_vm._v("\r\n          All\r\n        ")])])
+      _c("span", [_c("strong", [_vm._v("\n          All\n        ")])])
     ])
   }
 ]
@@ -42417,9 +42415,9 @@ var render = function() {
                   _vm._v(" "),
                   _c("h4", [
                     _vm._v(
-                      "\r\n                    " +
+                      "\n                    " +
                         _vm._s(restaurant.name) +
-                        "\r\n                  "
+                        "\n                  "
                     )
                   ]),
                   _vm._v(" "),
@@ -42448,7 +42446,7 @@ var staticRenderFns = [
       _vm._v(" "),
       _c("p", [
         _vm._v(
-          "\r\n                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptatibus?\r\n                "
+          "\n                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptatibus?\n                "
         )
       ])
     ])
@@ -57587,8 +57585,8 @@ var store = {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Fabio\Desktop\Boolean\classe 23\deliveboo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Fabio\Desktop\Boolean\classe 23\deliveboo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/boolean/laravel/deliveboo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/boolean/laravel/deliveboo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
